8225ed566ebfd3daf8d11dc37505d0790276d468
vladdumitru46 <45329135+vladdumitru46@users.noreply.github.com>
2023-03-24 14:37:17 +0200

    Add files via upload

---
+++ Sisteme de gestiue a bazelor de date/tema1/tema1/Form1.cs
@@ -0,0 +1,15 @@
+namespace tema1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void apasa_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
