d3e9a4ab086e1420bf8de8f92fa5c0a0f28086fa
vladdumitru46 <45329135+vladdumitru46@users.noreply.github.com>
2023-04-04 19:12:26 +0300

    Add files via upload

---
+++ Medii de proiectare si programare/C#/lab6/temaCSharp/repositoryes/interfaces/RepoShow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using temaCSharp.domains;
+
+namespace temaCSharp.repositoryes
+{
+    internal interface RepoShow: IRepository<String, Show>
+    {
+        IEnumerable<Show> searchArtist(DateTime date);
+    }
+}
