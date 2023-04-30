9b7a49061cb81952244f69be51f1f9fb0b20740c
vladdumitru46 <45329135+vladdumitru46@users.noreply.github.com>
2023-04-30 14:42:13 +0300

    Add files via upload

---
+++ Artificial intelligence/tema6/main.py
@@ -0,0 +1,7 @@
+from solution import execute
+from solutionWithoutBiblotecs import execute2
+
+if __name__ == '__main__':
+    execute("v1.csv", 'Economy..GDP.per.Capita.', "Freedom", 'Happiness.Score')
+    execute("v2.csv", 'Economy..GDP.per.Capita.', "Freedom", 'Happiness.Score')
+    execute("v3.csv", 'Economy..GDP.per.Capita.', "Freedom", 'Happiness.Score')
