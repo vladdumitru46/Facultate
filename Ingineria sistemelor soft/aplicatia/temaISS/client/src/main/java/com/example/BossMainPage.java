0bfcbb5cf56f68d21a419365df091ec1796742dc
vladdumitru46 <vladdumitru46@yahoo.com>
2023-05-11 16:44:14 +0300

    a

---
+++ Ingineria sistemelor soft/aplicatia/temaISS/client/src/main/java/com/example/BossMainPage.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+
+public class BossMainPage {
+    public void onSendTaskButton(ActionEvent actionEvent) {
+    }
+
+    public void onEmployeePerformancesPush(ActionEvent actionEvent) {
+    }
+
+    public void onActionsPush(ActionEvent actionEvent) {
+    }
+
+    public void onSendPush(ActionEvent actionEvent) {
+        
+    }
+}
---
+++ Ingineria sistemelor soft/aplicatia/temaISS/client/src/main/java/com/example/EmployeeArrivalPage.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+
+public class EmployeeArrivalPage {
+    public void onPresentPush(ActionEvent actionEvent) {
+    }
+}
