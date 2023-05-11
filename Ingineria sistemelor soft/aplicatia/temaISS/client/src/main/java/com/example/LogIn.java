15aa6ebf17dbd67e0e1823846439417190aa1654
vladdumitru46 <vladdumitru46@yahoo.com>
2023-05-11 16:30:36 +0300

    a

---
+++ Ingineria sistemelor soft/aplicatia/temaISS/client/src/main/java/com/example/LogIn.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+
+public class LogIn {
+    public void onLogInPush(ActionEvent actionEvent) {
+    }
+}
--- Ingineria sistemelor soft/aplicatia/temaISS/client/src/main/java/com/example/MainApplication.java
+++ Ingineria sistemelor soft/aplicatia/temaISS/client/src/main/java/com/example/MainApplication.java
@@ -1,4 +1,4 @@
-package com.example.temaiss;
+package com.example;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/logIn.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -18,6 +18,6 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        Application.launch();
     }
 }
