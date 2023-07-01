7469ed1407206001de90df3c65bb0900b3eb8a58
vladdumitru46 <45329135+vladdumitru46@users.noreply.github.com>
2023-07-01 17:00:40 +0300

    Delete JavaChatSockets directory

--- JavaChatSockets/ChatPersistence/src/main/java/chat/persistence/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package chat.persistence;
-
-import chat.model.Message;
-
-/**
- * Created by grigo on 3/17/17.
- */
-public interface MessageRepository extends ICrudRepository<Integer, Message> {
-}
--- JavaChatSockets/chat-rest-client/src/utils/consts.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by grigo on 5/19/17.
- */
-export const CHAT_USERS_BASE_URL='http://localhost:8080/chat/users';
