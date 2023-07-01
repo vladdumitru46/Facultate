c8590d9b3495b37edc698a1ded95876006a46ea6
vladdumitru46 <45329135+vladdumitru46@users.noreply.github.com>
2023-07-01 16:59:48 +0300

    Delete ChatProtobuff_v3 directory

--- ChatProtobuff_v3/ChatNetworkingProtobuff/src/main/java/chat/network/rpcprotocol/RequestType.java
+++
@@ -1,6 +0,0 @@
-package chat.network.rpcprotocol;
-
-
-public enum RequestType {
-    LOGIN, LOGOUT, GET_LOGGED_FRIENDS, SEND_MESSAGE;
-}
--- ChatProtobuff_v3/ChatNetworkingProtobuff/src/main/java/chat/network/rpcprotocol/ResponseType.java
+++
@@ -1,6 +0,0 @@
-package chat.network.rpcprotocol;
-
-
-public enum ResponseType {
-    OK, ERROR, GET_LOGGED_FRIENDS,UPDATE, NEW_MESSAGE, FRIEND_LOGGED_IN,FRIEND_LOGGED_OUT;
-}
