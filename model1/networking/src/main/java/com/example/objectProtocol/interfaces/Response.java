d2826501839839c8bc1bbb17aa879cdd0909af6d
vladdumitru46 <45329135+vladdumitru46@users.noreply.github.com>
2023-07-01 17:02:23 +0300

    Delete model1 directory

--- model1/networking/src/main/java/com/example/objectProtocol/interfaces/Response.java
+++
@@ -1,6 +0,0 @@
-package com.example.objectProtocol.interfaces;
-
-import java.io.Serializable;
-
-public interface Response extends Serializable {
-}
--- model1/persistence/src/main/java/com/example/interfaces/IRepoGame.java
+++
@@ -1,7 +0,0 @@
-package com.example.interfaces;
-
-import com.example.Game;
-
-public interface IRepoGame extends IRepository<Game, Integer> {
-
-}
