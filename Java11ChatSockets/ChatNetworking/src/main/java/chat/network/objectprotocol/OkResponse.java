11b5f1c9f2977d6cc7bd70d1d84cdfe07e0b4a8a
vladdumitru46 <45329135+vladdumitru46@users.noreply.github.com>
2023-07-01 17:00:24 +0300

    Delete Java11ChatSockets directory

--- Java11ChatSockets/ChatNetworking/src/main/java/chat/network/objectprotocol/OkResponse.java
+++
@@ -1,5 +0,0 @@
-package chat.network.objectprotocol;
-
-
-public class OkResponse implements Response{
-}
--- Java11ChatSockets/ChatPersistence/src/main/java/chat/persistence/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package chat.persistence;
-
-import chat.model.Message;
-
-
-public interface MessageRepository extends ICrudRepository<Integer, Message> {
-}
